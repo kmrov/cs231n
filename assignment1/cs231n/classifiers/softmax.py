0de5a2575f2e95dabf91e17e82f71e2f7b7899ef
Nikita Komarov <nikita@kmrov.ru>
2016-08-07 00:49:03 +0300

    Assignment 1, Q3 done.

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -22,17 +22,21 @@ def softmax_loss_naive(W, X, y, reg):
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+  for (i,x) in enumerate(X):
+    f = x.dot(W)
+    f -= np.max(f)
+    s = np.sum(np.exp(f))
+    loss -= np.log(np.exp(f[y[i]]) / s)
+    for k in xrange(num_classes):
+      pk = np.exp(f[k]) / s
+      dW[:, k] += (pk - (k == y[i])) * x
+  loss /= num_train
+  loss += 0.5 * reg * np.sum(W * W)
+  dW /= num_train
+  dW += reg*W
 
   return loss, dW
 
@@ -46,17 +50,24 @@ def softmax_loss_vectorized(W, X, y, reg):
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+  f = X.dot(W)
+  fmax = np.max(f, axis=1, keepdims=True)
+  f -= fmax
+  s = np.sum(np.exp(f), axis=1, keepdims=True)
+  p = np.exp(f) / s
+
+  loss = np.sum(-np.log(p[np.arange(num_train), y]))
+  loss /= num_train
+  loss += 0.5 * reg * np.sum(W * W)
+
+  i = np.zeros_like(p)
+  i[np.arange(num_train), y] = 1
+  dW = X.T.dot(p - i)
+
+  dW /= num_train
+  dW += reg * W
 
   return loss, dW
-
